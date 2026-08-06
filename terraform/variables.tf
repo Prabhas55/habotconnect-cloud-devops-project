63740806cab6e14523aeb4892ad01b37718bd8c7
Prabhas Nalajala <prabhaschowdary14@gmail.com>
2026-08-06 10:03:48 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,36 +1,69 @@
+###############################################################################
+# GCP Project Configuration
+###############################################################################
+
 variable "project_id" {
-  description = "GCP project ID"
-  type        = string
+
+  description = "Google Cloud Project ID"
+
+  type = string
+
 }
 
 variable "region" {
-  description = "GCP region for all resources"
-  type        = string
-  default     = "asia-south1"
+
+  description = "Google Cloud Region"
+
+  type = string
+
 }
 
 variable "environment" {
-  description = "Deployment environment (staging | production)"
-  type        = string
-  default     = "staging"
+
+  description = "Deployment Environment"
+
+  type = string
+
+  default = "staging"
+
 }
 
+###############################################################################
+# Service Accounts
+###############################################################################
+
 variable "ingestion_service_account" {
-  description = "Service account email used by the ingestion pipeline to write raw payloads into D0"
-  type        = string
+
+  description = "Service Account used for uploading raw student data"
+
+  type = string
+
 }
 
 variable "etl_service_account" {
-  description = "Service account email used by ETL jobs to read raw payloads from D0"
-  type        = string
+
+  description = "Service Account used by ETL process"
+
+  type = string
+
 }
 
+###############################################################################
+# BigQuery Users
+###############################################################################
+
 variable "data_platform_admin_email" {
-  description = "Email of the data platform owner (BigQuery OWNER role on D1)"
-  type        = string
+
+  description = "BigQuery Dataset Owner"
+
+  type = string
+
 }
 
 variable "analytics_reader_email" {
-  description = "Email of the analytics reader granted RLS-scoped READER access on D1"
-  type        = string
+
+  description = "Analytics User"
+
+  type = string
+
 }
